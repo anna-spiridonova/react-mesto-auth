e8b1df5e0feb2ee1e291fac85588952d2a32c69a
Anna Spiridonova <spiridonova.aniutka@yandex.ru>
2023-04-13 17:31:17 +0300

    настроить localStorage

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,62 +1,88 @@
 import { useState } from "react";
-import { Link } from 'react-router-dom'; 
+import { Link, useNavigate } from 'react-router-dom'; 
 import InfoTooltip from "./InfoTooltip";
+import * as auth from '../utils/auth';
 
-function Register() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+function Register(props) {
+  const [formValue, setformValue] = useState({
+    email: "",
+    password: ""
+  });
 
-  function handleEmailChange(evt) {
-    setEmail(evt.target.value);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const navigate = useNavigate();
+
+  function handleChange(evt) {
+    const {name, value} = evt.target;
+    setformValue({
+      ...formValue,
+      [name]: value
+    })
   }
-  function handlePasswordChange(evt) {
-    setPassword(evt.target.value);
-  }
 
-  // function handleSubmit(evt) {
-  //   evt.preventDefault();
-  //   props.onUpdateData({
-  //     email,
-  //     password,
-  //   });
-  // }
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    const { email, password } = formValue;
+    auth.register(email, password)
+      .then((res) => {
+        setIsSuccess(true);
+        props.openTooltip();
+        setTimeout(() => {
+          navigate("/sign-in")
+        }, 3000)
+      })
+      .catch((err) => {
+        setIsSuccess(false);
+        props.openTooltip();
+        console.log(err)
+      });
+  }
 
   return (
-    <div className="login__container">
-      <h2 className="login__title">Регистрация</h2>
-      <form
-        className="login__form"
-        name="register-form"
-        // onSubmit={props.onSubmit}
-      >
-        <input
-          type="email"
-          className="login__input"
-          name="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <input
-          type="password"
-          className="login__input"
-          name="password"
-          placeholder="Пароль"
-          required
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <button type="submit" className="button login_submit">
-          Зарегистрироваться
-        </button>
-      </form>
-      <p className="login__text">Уже зарегистрированы?
-        <span>
-          <Link to="/sign-in" className="link login__text"> Войти</Link>
-        </span>
-      </p>
-    </div>
+    <>
+      <InfoTooltip
+        name={"tooltip"}
+        isOpen={props.isOpen}
+        onClose={props.onClose}
+        isSuccess={isSuccess}
+      />
+      <div className="login__container">
+        <h2 className="login__title">Регистрация</h2>
+        <form
+          className="login__form"
+          name="register-form"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="email"
+            className="login__input"
+            name="email"
+            placeholder="Email"
+            required
+            value={formValue.email}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            className="login__input"
+            name="password"
+            placeholder="Пароль"
+            required
+            value={formValue.password}
+            onChange={handleChange}
+          />
+          <button type="submit" className="button login_submit">
+            Зарегистрироваться
+          </button>
+        </form>
+        <p className="login__text">Уже зарегистрированы?
+          <span>
+            <Link to="/sign-in" className="link login__text"> Войти</Link>
+          </span>
+        </p>
+      </div>
+    </>
   );
 }
 
